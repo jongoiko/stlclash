cc6476c4240d4388e64f681af1dc6ed01828be96
Jon Goikoetxea <jon.goikoetxea.m@gmail.com>
2026-05-25 17:50:11 +0200

    Add MountainCarContinuous tasks

--- stlclash/classic_control/__init__.py
+++ stlclash/classic_control/__init__.py
@@ -3,3 +3,7 @@ from pathlib import Path
 from stlclash.tasks import register_tasks
 
 register_tasks("Pendulum-v1", "pendulum", Path(__file__).parent.resolve())
+
+register_tasks(
+    "MountainCarContinuous-v0", "mountain_car", Path(__file__).parent.resolve()
+)
