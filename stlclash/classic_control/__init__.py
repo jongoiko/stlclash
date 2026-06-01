c15b373503a3f26c90820cfca56e66e6c1e16b72
Jon Goikoetxea <jon.goikoetxea.m@gmail.com>
2026-06-01 17:38:57 +0200

    Modify MountainCar: Do not terminate, only truncate

--- stlclash/classic_control/__init__.py
+++ stlclash/classic_control/__init__.py
@@ -1,9 +1,16 @@
 from pathlib import Path
 
+from gymnasium.envs.registration import register
+
 from stlclash.tasks import register_tasks
 
 register_tasks("Pendulum-v1", "pendulum", Path(__file__).parent.resolve())
 
+register(
+    "ContinuousMountainCar-v0",
+    entry_point="stlclash.classic_control.mountain_car:make_mountain_car_env",
+    max_episode_steps=500,
+)
 register_tasks(
-    "MountainCarContinuous-v0", "mountain_car", Path(__file__).parent.resolve()
+    "ContinuousMountainCar-v0", "mountain_car", Path(__file__).parent.resolve()
 )
